810714398e9dafee1ce47a8bee37f48a859d95fe
crsmnd <crsmnd@gmail.com>
2014-10-05 20:39:23 -0300

    First Tiger commit

--- Learning Structs/ViewController.swift
+++ Learning Structs/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tigerNameLabel: UILabel!
+    @IBOutlet weak var tigerAgeLabel: UILabel!
+    @IBOutlet weak var tigerBreedLabel: UILabel!
+    @IBOutlet weak var tigerImageOutput: UIImageView!
+    
+    var myTigers:[Tiger] = []
+    
+    let tigerNames = ["Leo", "Jack", "Vince", "Ricky", "Toga", "Ju", "Ale", "Una"]
+    let tigerAge = [1, 3, 6, 3, 6, 7, 9, 12]
+    let tigerBreed = ["Bengali", "Asian", "Asian", "African", "African", "Alien", "Bengali", "Asian"]
+    let tigerImages = ["BengalTiger.jpg", "IndochineseTiger.jpg", "Lion.jpg", "LionCub1.jpg", "LionCub2.jpeg", "Lioness.jpeg", "MalayanTiger.jpg", "SiberianTiger.jpg"]
+    
+    var currentTiger = Tiger()
+    
+    for var i = 0; i < tigerNames.count; i++ {
+        currentTiger.name = tigerNames[i]
+        currentTiger.age = tigerAge[i]
+        currentTiger.breed = tigerBreed[i]
+        currentTiger.image = UIImage(named: tigerImages[i])
+        myTigers.append(currentTiger)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +43,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextTigerButton(sender: UIBarButtonItem) {
+        var i = 0
+        if i < tigerNames.count {
+            i++
+            tigerNameLabel.text = myTigers[i].name
+            tigerAgeLabel.text = "\(myTigers[i].age)"
+            tigerBreedLabel.text = myTigers[i].breed
+            tigerImageOutput.image = myTigers[i].image
+        }
+    }
 
+    
+    
 }
 
---
+++ My Structs.swift
@@ -0,0 +1,18 @@
+//
+//  My Structs.swift
+//  Learning Structs
+//
+//  Created by Vincent on 05/10/2014.
+//  Copyright (c) 2014 VD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+    
+}
